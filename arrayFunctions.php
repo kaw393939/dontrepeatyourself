ea9bd597ac08ae21b408a87e6882dbb8ad8e1ec7
kaw393939 <keith@webizly.com>
2017-10-25 01:16:37 -0400

    Create arrayFunctions.php

---
+++ arrayFunctions.php
@@ -0,0 +1,13 @@
+<?php
+  //this is a group of array functions
+  class arrayFunctions {
+  //add static to make it global so you can call it without instantiation
+     static public function printArray($array) {
+  //Print_r is weird because it doesn't return a value it prints you need to add TRUE to have it return the output as string
+       return print_r($array, TRUE);
+     }
+     static public function arrayCount($array) {
+  //you can just return the output of the function
+        return count($array); 
+     }	
+  }
